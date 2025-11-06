7d74079f82739c8f61b027b44e2073a070b81c0c
Asedos08 <alamjeetsingh.dhaliwal@ontariotechu.net>
2025-11-06 14:43:19 -0500

    Added styling to the page, with the routes

--- New-Portfolio/routes/index.js
+++ New-Portfolio/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET home page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Portfolio' });
+});
+
+router.get('/projects', function(req, res, next) {
+  res.redirect('/#projects');  // Redirect to projects section
+});
+
+router.get('/contact', function(req, res, next) {
+  res.redirect('/#contact');  // Redirect to contact section
 });
 
 module.exports = router;
